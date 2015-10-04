e47346d8af33ce3590e75bc409140ed325ac2501
Lauricio Su <me@lauricio.com>
2015-10-04 19:36:35 +0700

    updated less dependency to 2.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:sortable',
-  summary: 'Sortable list components',
-  version: '1.0.0',
+  summary: 'TESTING_DO_NOT_USE Sortable list components',
+  version: '1.1.0',
   git: ' /* Fill me in! */ '
 });
 
@@ -10,14 +10,14 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.2.0.2');
   // Core
   api.use([
     'templating'
     ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@1.0.15','mquandalle:jade@0.4.1','rubaxa:sortable@1.1.0'
+    'lauricio:less-autoprefixer@2.5.0_3','mquandalle:jade@0.4.1','rubaxa:sortable@1.1.0'
     ]);
   api.addFiles('lib/oo-sortable.jade', C);
   api.addFiles('lib/oo-sortable.js', C);
